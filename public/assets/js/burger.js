5f52d0e301fbe31702ce2ebac9c9b0ae7ba3b01b
SK26 <saul.lazzarini@gmail.com>
2020-01-04 11:35:12 +0100

    100% working

--- public/assets/js/burger.js
+++ public/assets/js/burger.js
@@ -29,14 +29,16 @@ $(function() {
         .trim()
     };
 
-    // Send the POST request.
-    $.ajax("/api/burgers", {
-      type: "POST",
-      data: newBurger
-    }).then(function() {
-      console.log("created new burger");
-      // Reload the page to get the updated list
-      location.reload();
-    });
+    if (newBurger.name.length > 0) {
+      // Send the POST request.
+      $.ajax("/api/burgers", {
+        type: "POST",
+        data: newBurger
+      }).then(function() {
+        console.log("created new burger");
+        // Reload the page to get the updated list
+        location.reload();
+      });
+    }
   });
 });
